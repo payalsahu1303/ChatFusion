81a638a9fc0c6620957ba232f70ad80661966ec1
Payal Sahu <141853271+payalsahu1303@users.noreply.github.com>
2024-09-18 20:51:39 +0530

    Add files via upload

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,132 @@
+import { useContext } from "react";
+import { assets } from "../../assets/assets";
+import "./Main.css";
+import { Context } from "../../context/Context";
+const Main = () => {
+	const {
+		onSent,
+		recentPrompt,
+		showResults,
+		loading,
+		resultData,
+		setInput,
+		input,
+	} = useContext(Context);
+
+    const handleCardClick = (promptText) => {
+			setInput(promptText);
+		};
+	return (
+		<div className="main">
+			<div className="nav">
+				<div className="logo">
+                    <img src={assets.gemini_icon} alt="Logo" />
+                    <p>ChatFusion</p>
+                </div>
+				<img src={assets.user_icon} alt="" />
+			</div>
+			<div className="main-container">
+				{!showResults ? (
+					<>
+						<div className="greet">
+							<p>
+								<span>Hello ,</span>
+							</p>
+							<p>How Can i Help You ?</p>
+						</div>
+						<div className="cards">
+							<div
+								className="card"
+								onClick={() =>
+									handleCardClick("Suggest Some Place To Visit In Kerala")
+								}
+							>
+								<p>Suggest Some Place To Visit In Kerala </p>
+								<img src={assets.compass_icon} alt="" />
+							</div>
+							<div
+								className="card"
+								onClick={() =>
+									handleCardClick(
+										"Brainstorm team bonding activities for our work retreat"
+									)
+								}
+							>
+								<p>Brainstorm team bonding activities for our work retreat </p>
+								<img src={assets.message_icon} alt="" />
+							</div>
+							<div
+								className="card"
+								onClick={() =>
+									handleCardClick("How to Create a Gyroscope using Disc?")
+								}
+							>
+								<p>How to Create a Gyroscope using Disc?</p>
+								<img src={assets.bulb_icon} alt="" />
+							</div>
+							<div
+								className="card"
+								onClick={() => {
+									handleCardClick(
+										"Create a Script for the youtube video about coding "
+									);
+								}}
+							>
+								<p>Create a Script for the youtube video about coding </p>
+								<img src={assets.code_icon} alt="" />
+							</div>
+						</div>
+					</>
+				) : (
+					<div className="result">
+						<div className="result-title">
+							<img src={assets.user_icon} alt="" />
+							<p>{recentPrompt}</p>
+						</div>
+						<div className="result-data">
+							<img src={assets.gemini_icon} alt="" />
+							{loading ? (
+								<div className="loader">
+									<hr />
+									<hr />
+									<hr />
+								</div>
+							) : (
+								<p dangerouslySetInnerHTML={{ __html: resultData }}></p>
+							)}
+						</div>
+					</div>
+				)}
+
+				<div className="main-bottom">
+					<div className="search-box">
+						<input
+							onChange={(e) => {
+								setInput(e.target.value);
+							}}
+							value={input}
+							type="text"
+							placeholder="Enter the task/message here"
+						/>
+						<div>
+							<img
+								src={assets.send_icon}
+								alt=""
+								onClick={() => {
+									onSent();
+								}}
+							/>
+						</div>
+					</div>
+					<div className="bottom-info">
+						<p>
+                        Connecting you to the information you need, when you need it. © 2024 ChatFusion. All rights reserved.
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Main;
---
+++ src/context/Context.jsx
@@ -0,0 +1,84 @@
+import { createContext, useState } from "react";
+import runChat from "../config/chatfusion";
+
+export const Context = createContext();
+
+const ContextProvider = (props) => {
+	const [input, setInput] = useState("");
+	const [recentPrompt, setRecentPrompt] = useState("");
+	const [prevPrompts, setPrevPrompts] = useState([]);
+	const [showResults, setShowResults] = useState(false);
+	const [loading, setLoading] = useState(false);
+	const [resultData, setResultData] = useState("");
+
+	const delayPara = (index, nextWord) => {
+		setTimeout(function () {
+			setResultData((prev) => prev + nextWord);
+		}, 10 * index);
+	};
+    const newChat = () =>{
+        setLoading(false);
+        setShowResults(false)
+    }
+
+	const onSent = async (prompt) => {
+		setResultData("");
+		setLoading(true);
+		setShowResults(true);
+        let response;
+        if(prompt !== undefined){
+            response = await runChat(prompt);
+            setRecentPrompt(prompt)
+        }else{
+            setPrevPrompts(prev=>[...prev,input]);
+            setRecentPrompt(input);
+            response=await runChat(input);
+        }
+		
+		try {
+			
+			
+			let responseArray = response.split("**");
+            let newResponse = "";
+			for (let i = 0; i < responseArray.length; i++) {
+				if (i === 0 || i % 2 !== 1) {
+					newResponse += responseArray[i];
+				} else {
+					newResponse += "<b>" + responseArray[i] + "</b>";
+				}
+			}
+			let newResponse2 = newResponse.split("*").join("<br/>");
+			let newResponseArray = newResponse2.split("");
+			for (let i = 0; i < newResponseArray.length; i++) {
+				const nextWord = newResponseArray[i];
+				delayPara(i, nextWord + "");
+			}
+		} catch (error) {
+			console.error("Error while running chat:", error);
+			// Handle error appropriately
+		} finally {
+			setLoading(false);
+			setInput("");
+		}
+	};
+
+	const contextValue = {
+		prevPrompts,
+		setPrevPrompts,
+		onSent,
+		setRecentPrompt,
+		recentPrompt,
+		input,
+		setInput,
+		showResults,
+		loading,
+		resultData,
+		newChat,
+	};
+
+	return (
+		<Context.Provider value={contextValue}>{props.children}</Context.Provider>
+	);
+};
+
+export default ContextProvider;
